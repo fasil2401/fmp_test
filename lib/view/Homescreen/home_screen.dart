bf1150b39ca702100993fbd969b0b7ab566d42b9
Salman Salim <nssalman97@gmail.com>
2024-05-24 20:19:32 +0400

    home scren start

--- lib/view/Homescreen/home_screen.dart
+++ lib/view/Homescreen/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fmp_test/controller/home_controller.dart';
 import 'package:fmp_test/services/snackbar_servises.dart';
 import 'package:fmp_test/utils/asset_paths.dart';
+import 'package:fmp_test/utils/colors.dart';
 import 'package:fmp_test/utils/routes.dart';
 import 'package:fmp_test/view/componets/button_widget.dart';
+import 'package:fmp_test/view/componets/components.dart';
+import 'package:fmp_test/view/componets/text_widget.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,34 +20,47 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(AppImages.home_back),
-              fit: BoxFit.cover,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.home_back),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          width: size.width,
-          height: size.height,
-          child: Center(
-            child: GetBuilder<HomeController>(
-              builder: (_) {
-                return BottonWidget(
-                  text: 'LOGOUT',
-                  isLoading: _.isLoading.value,
-                  onPressed: () async {
-                    var responce = await homeController.logoutUser();
-                    if (responce['isSuccess'] == false) {
-                      Get.showSnackbar(getxSnackbar(
-                          message: responce['Message'], isError: true));
-                    } else {
-                      Get.offNamed(RouteManager.login);
-                    }
-                  },
-                );
-              }
-            ),
-          ),
-        ),
+            width: size.width,
+            height: size.height,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(color: AppColors.primary),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: Center(
+                              child:
+                                  SvgPicture.asset('assets/icons/Home-3.svg'),
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextWidget(
+                                  style: AppTextStyles.greyHeadBold
+                                      .copyWith(color: Colors.white),
+                                  text: 'Location')
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
     );
   }
